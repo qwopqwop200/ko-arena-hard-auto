d3d11efdf0adb5e9ee7ffa7b5464a495466a24e1
qwopqwop200 <qwopqwop200@gmail.com>
2024-12-12 21:31:36 +0900

    'fix-load-question-bug'

--- utils.py
+++ utils.py
@@ -57,7 +57,7 @@ temperature_config = {
 def load_questions(question_file: str):
     """Load questions from a file."""
     questions = []
-    with open(question_file, "r") as ques_file:
+    with open(question_file, "r", encoding='utf-8') as ques_file:
         for line in ques_file:
             if line:
                 questions.append(json.loads(line))
